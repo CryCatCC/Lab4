69655a9b72934ca06af018bfab4fe11d54b83374
CryCatCC <160016979+CryCatCC@users.noreply.github.com>
2024-04-12 15:48:03 +0300

    Update library.js

--- library.js
+++ library.js
@@ -66,123 +66,151 @@
 
 
     //Сортування методом вставок
-    sortArrays.insertionSort = function (arr, ascending = true, debug = false) {
-        let comparisons = 0;
-        let movements = 0;
+   sortArrays.insertionSort = function (arr, ascending = true, debug = false) {
+    let comparisons = 0;
+    let movements = 0;
+   
+    let filteredArr = arr.filter(element => element !== undefined);
+    
+  
+    for (let i = 1; i < filteredArr.length; i++) {
+        let current = filteredArr[i];
+        let j = i - 1;
 
-        for (let i = 1; i < arr.length; i++) {
-            let current = arr[i];
-            let j = i - 1;
-
-            while (j >= 0 && (current < arr[j] && ascending || current > arr[j] && !ascending)) {
-                comparisons++;
-                arr[j + 1] = arr[j];
-                j--;
-                movements++;
-            }
-
-            arr[j + 1] = current;
+      
+        while (j >= 0 && (ascending ? filteredArr[j] > current : filteredArr[j] < current)) {
+            comparisons++;
+            filteredArr[j + 1] = filteredArr[j];
+            movements++;
+            j--;
         }
 
-        if (debug) {
-            console.log("Insertion Sort:");
-            console.log("Number of comparisons:", comparisons);
-            console.log("Number of element movements:", movements);
-        }
-
-        return arr;
+        filteredArr[j + 1] = current;
     }
 
+  
+    let sortedIndex = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== undefined) {
+            arr[i] = filteredArr[sortedIndex];
+            sortedIndex++;
+        }
+    }
 
+    if (debug) {
+        console.log("Insertion Sort:");
+        console.log("Number of comparisons:", comparisons);
+        console.log("Number of element movements:", movements);
+    }
+
+    return arr;
+}
 
     //Сортування методом Шелла
-    sortArrays.shellSort = function (arr, ascending = true, debug = false) {
-        let comparisons = 0;
-        let exchanges = 0;
+ sortArrays.shellSort = function (arr, ascending = true, debug = false) {
+    let comparisons = 0;
+    let exchanges = 0;
 
-        const n = arr.length;
-        let gap = Math.floor(n / 2);
+  
+    let filteredArr = arr.filter(element => element !== undefined);
+    const n = filteredArr.length;
+    let gap = Math.floor(n / 2);
 
-        while (gap > 0) {
-            for (let i = gap; i < n; i++) {
-                let temp = arr[i];
-                let j = i;
+  
+    while (gap > 0) {
+        for (let i = gap; i < n; i++) {
+            let temp = filteredArr[i];
+            let j = i;
 
-                while (j >= gap && (temp < arr[j - gap] && ascending || temp > arr[j - gap] && !ascending)) {
-                    comparisons++;
-                    arr[j] = arr[j - gap];
-                    j -= gap;
-                    exchanges++;
-                }
-
-                arr[j] = temp;
+            while (j >= gap && (ascending ? filteredArr[j - gap] > temp : filteredArr[j - gap] < temp)) {
+                comparisons++;
+                filteredArr[j] = filteredArr[j - gap];
+                j -= gap;
+                exchanges++;
             }
 
-            gap = Math.floor(gap / 2);
+            filteredArr[j] = temp;
         }
 
-        if (debug) {
-            console.log("Shell Sort:");
-            console.log("Number of comparisons:", comparisons);
-            console.log("Number of exchanges:", exchanges);
-        }
-
-        return arr;
+        gap = Math.floor(gap / 2);
     }
 
+   
+    let sortedIndex = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== undefined) {
+            arr[i] = filteredArr[sortedIndex];
+            sortedIndex++;
+        }
+    }
 
+    if (debug) {
+        console.log("Shell Sort:");
+        console.log("Number of comparisons:", comparisons);
+        console.log("Number of exchanges:", exchanges);
+    }
 
+    return arr;
+}
     //Сортування методом Хоара
-    sortArrays.quickSort = function (arr, ascending = true, debug = false) {
-        let comparisons = 0;
-        let exchanges = 0;
+   sortArrays.quickSort = function (arr, ascending = true, debug = false) {
+    let comparisons = 0;
+    let exchanges = 0;
+    function partition(arr, low, high) {
+        let pivot = arr[Math.floor((low + high) / 2)];
+        let i = low;
+        let j = high;
 
-        function partition(arr, low, high) {
-            let pivot = arr[Math.floor((low + high) / 2)];
-            let i = low;
-            let j = high;
-
-            while (i <= j) {
-                while ((arr[i] < pivot && ascending) || (arr[i] > pivot && !ascending)) {
-                    comparisons++;
-                    i++;
-                }
-                while ((arr[j] > pivot && ascending) || (arr[j] < pivot && !ascending)) {
-                    comparisons++;
-                    j--;
-                }
-
-                if (i <= j) {
-                    let temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    exchanges++;
-                    i++;
-                    j--;
-                }
+        while (i <= j) {
+            while ((arr[i] < pivot && ascending) || (arr[i] > pivot && !ascending)) {
+                comparisons++;
+                i++;
+            }
+            while ((arr[j] > pivot && ascending) || (arr[j] < pivot && !ascending)) {
+                comparisons++;
+                j--;
             }
 
-            return i;
-        }
-
-        function sort(arr, low, high) {
-            if (low < high) {
-                let pi = partition(arr, low, high);
-                sort(arr, low, pi - 1);
-                sort(arr, pi, high);
+            if (i <= j) {
+              
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                exchanges++;
+                i++;
+                j--;
             }
         }
 
-        sort(arr, 0, arr.length - 1);
-
-        if (debug) {
-            console.log("Quick Sort:");
-            console.log("Number of comparisons:", comparisons);
-            console.log("Number of exchanges:", exchanges);
-        }
-
-        return arr;
+        return i;
     }
+    function sort(arr, low, high) {
+        if (low < high) {
+            let pi = partition(arr, low, high);
+            sort(arr, low, pi - 1);
+            sort(arr, pi, high);
+        }
+    }
+
+    let filteredArr = arr.filter(element => element !== undefined);
+
+    sort(filteredArr, 0, filteredArr.length - 1);
+
+    let sortedIndex = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== undefined) {
+            arr[i] = filteredArr[sortedIndex];
+            sortedIndex++;
+        }
+    }
+    if (debug) {
+        console.log("Quick Sort:");
+        console.log("Number of comparisons:", comparisons);
+        console.log("Number of exchanges:", exchanges);
+    }
+
+    return arr;
+};
 
     window.sortArrays = sortArrays;
 
